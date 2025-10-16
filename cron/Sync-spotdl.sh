2973c7a09c35444b52fa0de81cf9faa850c8ec9b
Andreius_14 <Andreius_14@outlook.com>
2025-10-15 19:42:02 -0500

    (fix) Mejor Orden,  Vista Clara, Descarga como debe, Notifica al Usuario

--- cron/Sync-spotdl.sh
+++ cron/Sync-spotdl.sh
@@ -1,42 +1,55 @@
 #!/bin/bash
-source __Shared-Cron.sh
-# ---------------------------------------------------------------------------
 
-# ═══════════════════════════════
-#           Variables
-# ═══════════════════════════════
-ruta_Error="/home/carlos/Documentos/Sync-spotdl.log"
+#════════════════════════════════════════════════════════════════════
+#                   Habilitando Modo Grafico
+#════════════════════════════════════════════════════════════════════
+# Variables necesarias para que cron hable con tu sesión gráfica
+export DISPLAY=:0
+export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u)/bus
+export XAUTHORITY="$HOME/.Xauthority" # <-- esta asegura el acceso a tu sesión
 
-ruta_prueba="/home/carlos/Música/"
+#════════════════════════════════════════════════════════════════════
+#                  Variables de configuración
+#════════════════════════════════════════════════════════════════════
+ruta_log="$HOME/Documentos/Sync-spotdl.log"
+ruta_prueba="$HOME/Música/"
 ruta_playlist="/media/carlos/Personal/101__Musica/Spotify - Mis Playlist/"
-# ═══════════════════════════════
-#           Funciones
-# ═══════════════════════════════
 
-msm() {
-    echo "==== $(date '+%Y-%m-%d %H:%M:%S') → Sincronizando $1 ====" >>"$ruta_Error"
+spotdl="$HOME/.local/bin/spotdl"
+
+#════════════════════════════════════════════════════════════════════
+#                       Function
+#════════════════════════════════════════════════════════════════════
+msm_log() {
+    echo "==== $(date '+%Y-%m-%d %H:%M:%S') → Sincronizando $1 ====" >>"$ruta_log"
 }
 
-spotdl_pull() {
-    local rutaPrincipal=$1
-    local spotdl="/home/carlos/.local/bin/spotdl"
+msm(){ 
+    /usr/bin/notify-send "Sincronizacion Spotdl - $1" 
+}
 
-    # Bucle
+run() {
+    local ruta=$1
+    #Bucle
     while IFS= read -r -d '' ruta_File; do
-        # VAR
-        local ruta_Parent="$($dirname "$ruta_File")"
-        local formato=$($ls "$ruta_Parent" 2>/dev/null | $grep -Eo "(opus|mp3|m4a|ogg)$" | $sort | $uniq -c | $sort -nr | $head -n1 | $awk '{print $2}')
 
-        # Verificado
+        #Variables
+        local ruta_Parent="$(dirname "$ruta_File")"
+        local name_Parent="$(basename "$ruta_Parent")"
+        local formato=$(ls "$ruta_Parent" | grep -Eo "(opus|mp3|m4a|ogg)$" | sort | uniq -c | sort -nr | head -n1 | awk '{print $2}')
         [[ -z "$formato" ]] && formato="opus"
 
-        if $spotdl --format "$formato" sync "$ruta_File" --overwrite skip --output "$ruta_Parent" >>"$ruta_Error" 2>&1; then
-            /usr/bin/notify-send "Spotdl ✅" "Sincronizado: $($basename "$ruta_File")"
+        #Ejecucion
+        msm_log "$name_Parent (formato: $formato)"
+
+        if $spotdl --format "$formato" sync "$ruta_File" --overwrite skip --output "$ruta_Parent" >>"$ruta_log" 2>&1; then
+            msm "✅ $name_Parent completado"
         else
-            /usr/bin/notify-send "Spotdl ❌" "Error en: $($basename "$ruta_File")"
+            msm "❌ Error en $name_Parent"
         fi
 
-    done < <($find "$rutaPrincipal" -name "*.spotdl" -print0 2>/dev/null) # Maneja si no hay
+    done < <(find "$ruta" -name "*.spotdl" -print0 2>/dev/null) # Maneja si no hay
+
 
 }
 
@@ -44,13 +57,18 @@ spotdl_pull() {
 #             clean
 # ═══════════════════════════════
 
-[ -f "$ruta_Error" ] && [ $(stat -c%s "$ruta_Error") -gt 5000000 ] && : >"$ruta_Error"
+[ -f "$ruta_log" ] && [ $(stat -c%s "$ruta_log") -gt 5000000 ] && : >"$ruta_log"
 
-# ═══════════════════════════════
-#             Main
-# ═══════════════════════════════
+#════════════════════════════════════════════════════════════════════
+#                      Ejecucion de Comandos
+#════════════════════════════════════════════════════════════════════
+
+
 # msm "Prueba"
-# spotdl_pull "$ruta_prueba"
+# run "$ruta_prueba"
 
 msm "Playlist"
-spotdl_pull "$ruta_playlist"
+run "$ruta_playlist"
+
+msm "Proceso completado"
+msm_log "Fin de sincronización"
